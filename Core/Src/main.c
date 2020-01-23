2b8fa6f774c54a2572859cb8349f144b9939a061
Sideous <jvanmuck@yahoo.com>
2020-01-22 21:14:24 -0800

    further updates to set_duty_cycle()

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -632,7 +632,33 @@ void process_keystroke()
 
 }
 static void set_pulse_duty_cycle(TIM_HandleTypeDef *htim,TIM_OC_InitTypeDef *sConfigOC, int dc)
-{}
+{
+	 /* USER CODE END TIM3_Init 1 */
+	  htim3.Instance = TIM3;
+	  htim3.Init.Prescaler = 16000;
+	  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+	  htim3.Init.Period = 5000;
+	  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  sConfigOC->OCMode = TIM_OCMODE_PWM1;
+	  sConfigOC->Pulse = 1400;
+	  sConfigOC->OCPolarity = TIM_OCPOLARITY_HIGH;
+	  sConfigOC->OCFastMode = TIM_OCFAST_DISABLE;
+	  if (HAL_TIM_PWM_ConfigChannel(&htim3, sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+}
 /* USER CODE END 4 */
 
 /**
